5419edbd40838b5c1c168c52f95d07008ad6f2d1
Mario Aldayuz <36048374+marioaldayuz@users.noreply.github.com>
2024-02-25 17:18:07 -0500

    Update password_protect.js

--- password_protect.js
+++ password_protect.js
@@ -6,18 +6,19 @@ body {
 </style>
 
 <script type="text/javascript">
-      // PUT THIS IN THE BODY SECTION OF THE FUNNEL SETTINGS
+      // PUT THIS IN THE BODY TRACKING SECTION
       (function() {
         var correctPassword = 'yourPassword'; // Replace 'yourPassword' with the actual password
         var userInput = prompt('Please enter the password to access this page:');
 
         if(userInput === correctPassword) {
           // Correct password: Remove the style hiding the body
-          document.body.style.display = 'block';
+          document.body.style= 'display: block !important';
         } else {
           // Incorrect password: Optionally, clear the body content or redirect
           alert('Access Denied');
           document.body.innerHTML = ''; // Clear the body content
+          // window.location.href = 'error_page.html'; // Or redirect to another page
         }
       })();
     </script>
