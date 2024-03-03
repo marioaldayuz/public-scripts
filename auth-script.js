2bf9cbface4794d7081d29cea73b2dc4fc81292a
Mario Aldayuz <36048374+marioaldayuz@users.noreply.github.com>
2024-03-03 14:38:13 -0500

    Update auth-script.js
    refactored

--- auth-script.js
+++ auth-script.js
@@ -1,40 +1,48 @@
+const API_URL = 'https://services.leadconnectorhq.com/oauth/token';
+const CLIENT_ID = 'YOURCLIENTIDHERE';
+const CLIENT_SECRET = 'YOURCLIENTSECRETHERE';
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
 async function handleRequest(request) {
-  if (request.method === 'POST') {
-    const body = await request.text()
-    const parsedBody = new URLSearchParams(body)
-
-    // Extract code, grant_type, and refresh_token from request body
-    const code = parsedBody.get('code')
-    const grantType = parsedBody.get('grant_type')
-    const refreshToken = parsedBody.get('refresh_token')
-
-    // Create POST request options with extracted fields and constants
-    const url = 'https://services.leadconnectorhq.com/oauth/token'
-    const options = {
-      method: 'POST',
-      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-      body: new URLSearchParams({
-        client_id: 'YOURCLIENTIDHERE',
-        client_secret: 'YOURCLIENTSECRETHERE',
-        grant_type: grantType,
-        code: code,
-        refresh_token: refreshToken
-      })
-    }
-
-    try {
-      const response = await fetch(url, options)
-      const data = await response.json()
-      return new Response(JSON.stringify(data), {status: 200})
-    } catch (error) {
-      console.error(error)
-      return new Response('Error', {status: 500})
-    }
-  } else {
-    return new Response('Method not allowed', {status: 405})
+  if (request.method !== 'POST') {
+    return new Response('Method not allowed', {status: 405});
   }
+
+  try {
+    const {code, grantType, refreshToken} = await parseRequestBody(request);
+    const options = createPostRequestOptions(grantType, code, refreshToken);
+    const response = await fetch(API_URL, options);
+    const data = await response.json();
+    return new Response(JSON.stringify(data), {status: 200});
+  } catch (error) {
+    console.error(error);
+    return new Response(`Error: ${error.message}`, {status: 500});
+  }
+}
+
+async function parseRequestBody(request) {
+  const body = await request.text();
+  const parsedBody = new URLSearchParams(body);
+  return {
+    code: parsedBody.get('code'),
+    grantType: parsedBody.get('grant_type'),
+    refreshToken: parsedBody.get('refresh_token'),
+  };
+}
+
+function createPostRequestOptions(grantType, code, refreshToken) {
+  return {
+    method: 'POST',
+    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+    body: new URLSearchParams({
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      grant_type: grantType,
+      code: code,
+      refresh_token: refreshToken,
+    })
+  };
 }
